17a8e5e899dc8c7ecb026da11fc44c1828f2a82a
JAGM89 <joel.ag2004@gmail.com>
2025-08-19 22:53:43 -0600

    final

--- src/components/CalcularDescuento.jsx
+++ src/components/CalcularDescuento.jsx
@@ -1,48 +1,72 @@
+import UseCalcularDescuento from "../hooks/UseCalcularDescuento";
 
 const CalcularDescuento= () => {
+        const{
+    producto1,
+    setProducto1,
+    producto2,
+    setProducto2,
+    producto3,
+    setProducto3,
+    producto4,
+    setProducto4,
+    producto5,
+    setProducto5,
+    descuento,
+    setDescuento,
+    subtotal,
+    setSubTotal,
+    total,
+    setTotal,
+    Calcular,
+    Limpiar,
+        }=UseCalcularDescuento();
     
     return(
 
        <div className="container mt-5">
         <h2>Calculadora de Descuentos</h2>
+     
             <div className="col-6 mb-3">
                 <label className="form-label">Ingrese Producto 1: </label>
-                <input type="number" className="form-control" />
+                <input type="number" className="form-control" onChange={(e)=>setProducto1(e.target.value)} />
             </div>
             <div className="col-6 mb-3">
                 <label className="form-label">Ingrese Producto 2: </label>
-                <input type="number" className="form-control" />
+                <input type="number" className="form-control" onChange={(e)=>setProducto2(e.target.value)}/>
             </div>
             <div className="col-6 mb-3">
                 <label className="form-label">Ingrese Producto 3: </label>
-                <input type="number" className="form-control" />
+                <input type="number" className="form-control" onChange={(e)=>setProducto3(e.target.value)} />
             </div>
             <div className="col-6 mb-3">
                 <label className="form-label">Ingrese Producto 4: </label>
-                <input type="number" className="form-control" />
+                <input type="number" className="form-control" onChange={(e)=>setProducto4(e.target.value)}/>
             </div>
             <div className="col-6 mb-3">
                 <label className="form-label">Ingrese Producto 5: </label>
-                <input type="number" className="form-control" />
+                <input type="number" className="form-control" onChange={(e)=>setProducto5(e.target.value)}/>
             </div>
             <div className="col-6 mb-3">
                 <label className="form-label">Sub Total: </label>
-                <input type="number" className="form-control" readOnly/>
+                <input className="form-control" value={subtotal} readOnly/>
             </div>
             <div className="col-6 mb-3">
                 <label className="form-label">Descuento: </label>
-                <input type="number" className="form-control" readOnly/>
+                <input className="form-control" value={descuento} readOnly/>
             </div>
             <div className="col-6 mb-3">
                 <label className="form-label">Total a Pagar: </label>
-                <input type="number" className="form-control" readOnly/>
+                <input className="form-control" value={total} readOnly/>
             </div>
-
+            <div className="col-6 mb-3">
+                <button className="btn btn-success" onClick={Calcular}>Calcular</button>
+                <button className="btn btn-danger" onClick={Limpiar}>Limpiar</button>
+            </div>
+            
+ 
        </div>
     )
-
-
-
 
 };
 
--- src/hooks/UseCalcularDescuento.js
+++ src/hooks/UseCalcularDescuento.js
@@ -1,51 +1,103 @@
-import  { useState } from "react";
+import { useState } from "react";
 import Swal from "sweetalert2";
 
-const CalculadoraDescuento = () => {
-  const [productos, setProductos] = useState(Array(5).fill("")); // 5 productos
-  const [subtotal, setSubtotal] = useState(0);
-  const [descuentoPorcentaje, setDescuentoPorcentaje] = useState(0);
-  const [descuento, setDescuento] = useState(0);
-  const [total, setTotal] = useState(0);
+const UseCalcularDescuento = () => {
+  const [producto1, setProducto1] = useState('');
+  const [producto2, setProducto2] = useState('');
+  const [producto3, setProducto3] = useState('');
+  const [producto4, setProducto4] = useState('');
+  const [producto5, setProducto5] = useState('');
+  const [descuento, setDescuento] = useState('');
+  const [subtotal, setSubTotal] = useState('');
+  const [total, setTotal] = useState('');
 
-  const calcularDescuento = () => {
-    // Validar los productos
-    for (let value of productos) {
-      if (value === "" || isNaN(value) || value < 0) {
-        Swal.fire(
-          "Error",
-          "Por favor ingrese valores numéricos válidos en todos los campos.",
-          "error"
-        );
-        return;
-      }
+  const Calcular = () => {
+    if (producto1 === '') {
+      Alerta('Ingrese Producto 1');
+    } else if (producto2 === '') {
+      Alerta('Ingrese Producto 2');
+    } else if (producto3 === '') {
+      Alerta('Ingrese Producto 3');
+    } else if (producto4 === '') {
+      Alerta('Ingrese Producto 4');
+    } else if (producto5 === '') {
+      Alerta('Ingrese Producto 5');
+    }else {
+       let subtotal=Number(producto1)+Number(producto2)+Number(producto3)+Number(producto4)+Number(producto5);
+        setSubTotal(subtotal);   
+
+        let descuento=0;
+        if (subtotal>0 && subtotal<999.99) {
+            descuento=0
+        } else if (subtotal>1000 && subtotal<4999.99) {
+            descuento=10
+        }else if (subtotal>5000 && subtotal<8999.99) {
+            descuento=20
+        }else if (subtotal>9000 && subtotal<12999.99) {
+            descuento=20
+        }else if (subtotal>13000) {
+            descuento=30
+        }
+        setDescuento(descuento)
+        let descuentoporc=descuento/100
+        let totalsindes=subtotal*descuentoporc
+        let total=subtotal-totalsindes
+        setTotal(total)
+
+
+
     }
 
-    const valores = productos.map((p) => parseFloat(p));
-    const sub = valores.reduce((acc, val) => acc + val, 0);
+};
 
-    let porc = 0;
-    if (sub >= 1000 && sub < 5000) porc = 10;
-    else if (sub >= 5000 && sub < 9000) porc = 20;
-    else if (sub >= 9000 && sub < 13000) porc = 30;
-    else if (sub >= 13000) porc = 40;
 
-    const desc = (sub * porc) / 100;
-    const tot = sub - desc;
+  const Limpiar = () => {
+    setProducto1("");
+    setProducto2('');
+    setProducto3('');
+    setProducto4('');
+    setProducto5('');
+    setDescuento('');
+    setSubTotal('');
+    setTotal('');
 
-    setSubtotal(sub);
-    setDescuentoPorcentaje(porc);
-    setDescuento(desc);
-    setTotal(tot);
   };
 
-  const limpiar = () => {
-    setProductos(Array(5).fill(""));
-    setSubtotal(0);
-    setDescuentoPorcentaje(0);
-    setDescuento(0);
-    setTotal(0);
-  };
-   };
+  /**
+   * Muestra una alerta de advertencia
+   * @param {string} mensaje
+   */
 
-   export default CalculadoraDescuento;
+  const Alerta = (mensaje) => {
+    Swal.fire({
+      title: "Advertencia",
+      text: mensaje,
+      icon: "warning",
+    });
+  };
+
+  return{
+    producto1,
+    setProducto1,
+    producto2,
+    setProducto2,
+    producto3,
+    setProducto3,
+    producto4,
+    setProducto4,
+    producto5,
+    setProducto5,
+    descuento,
+    setDescuento,
+    subtotal,
+    setSubTotal,
+    total,
+    setTotal,
+    Calcular,
+    Limpiar,
+  }
+
+
+}
+
+export default UseCalcularDescuento;
